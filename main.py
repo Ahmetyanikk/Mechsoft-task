976f412c771fb440295f5e6fdd1c672ffcd79bd1
Ahmet Yanık <73365349+Ahmetyanikk@users.noreply.github.com>
2023-12-15 16:56:30 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+# main.py
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import firebase_admin
+from firebase_admin import firestore
+from firebase_admin import credentials
+
+app = firebase_admin.initialize_app()
+db = firestore.client()
+app = FastAPI()
+
+cred = credentials.Certificate('path/to/serviceAccount.json')
+
+app = firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+# Enable CORS (Cross-Origin Resource Sharing)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins for simplicity; update as needed for security
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
+
+class MeetingData(BaseModel):
+    ToplantiKonusu: str
+    Tarih: str
+    BaslangicSaati: str
+    BitisSaati: str
+    Katilimcilar: list
+
+@app.post("/submit")
+def submit_meeting_data(meeting_data: MeetingData):
+    # Process the meeting data as needed (e.g., save it to a database)
+    # For now, just print it
+    print("Meeting Data Received:")
+    print(meeting_data.dict())
+
+    return {"message": "Meeting data received successfully!"}
